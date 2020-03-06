d4627b9cbcf94a7b4f86f66ab177e9108f5ef5cc
iamadim <adim4usall@gmail.com>
2020-03-06 17:36:58 -0500

    Updates

---
+++ precache-manifest.b26fdf05c333a874481df838589fcd4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3f883c766665bffdaa5583670daf3f",
+    "url": "/royal-clothings/index.html"
+  },
+  {
+    "revision": "062f13c4105f3eefcbf6",
+    "url": "/royal-clothings/static/css/main.53172fbd.chunk.css"
+  },
+  {
+    "revision": "2433e7638b1f14ba4b42",
+    "url": "/royal-clothings/static/js/2.bde98980.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/royal-clothings/static/js/2.bde98980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062f13c4105f3eefcbf6",
+    "url": "/royal-clothings/static/js/main.5d385b30.chunk.js"
+  },
+  {
+    "revision": "2f3ec69c310e94b2f2a6",
+    "url": "/royal-clothings/static/js/runtime-main.3b288d1a.js"
+  }
+]);
