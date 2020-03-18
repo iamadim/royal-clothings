2ac8d68bc500925d5d9cf731358cab95eb72524d
iamadim <adim4usall@gmail.com>
2020-03-18 15:42:37 -0400

    Add files via upload

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+import './collection-item.styles.scss';
+/* jshint ignore:start */
+
+
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+    <div className="collection-item">
+        <div className="image"
+        style={{
+            backgroundImage: `url(${imageUrl})`
+        }}
+        />
+
+        <div className="collection-footer">
+            <span className="name">{name}</span>
+            <span className="price">{price}</span>
+        </div>
+    </div>
+);
+
+export default CollectionItem;
