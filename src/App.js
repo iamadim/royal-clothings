bb245808635adb22d7d44c54035a6b90817dacbb
iamadim <adim4usall@gmail.com>
2020-07-10 01:09:57 -0400

    implemented firebase utils, ability to store data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import Header from './components/header/header.component.jsx';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -25,10 +25,29 @@ class App extends React.Component {
   unsubscribeFromAuth = null;   // by default equals to null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        
+        userRef.onSnapshot(snapShot => {
+          this.setState(
+            {
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, 
+          () => {
+            console.log(this.state)
+          });
+        });
+         
+      }
+   
+        this.setState({ currentUser : userAuth});
+    
+    });
   }
 
   componentWillUnmount () {
